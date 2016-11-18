d5b6d082728b758c7d73a1eb5855c9f821aeb27f
ZaneWoodard <zwoodard@hawaii.edu>
2016-11-17 15:19:02 -1000

    Add more kanban features
    Tickbox changed to Done/Reopen button for better aesthetics.
    Delete button changed to also use a bootstrap btn class for consistency
    Can view task description by clicking the task

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -1,18 +1,31 @@
-import { Template } from 'meteor/templating';
-
-import { Tasks } from '../api/tasks.js';
+import {Template} from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var'
+import {Tasks} from '../api/tasks.js';
 
 import './task.html';
 
-Template.task.events({
-  'click .toggle-checked'() {
-    // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {
-      $set: { checked: ! this.checked },
-    });
-  },
-  'click .delete'() {
-    Tasks.remove(this._id);
-  },
+Template.task.onCreated(function timerOnCreated() {
+    this.showDescription = new ReactiveVar(false);
 });
 
+Template.task.events({
+    'click .toggle-checked'() {
+        // Set the checked property to the opposite of its current value
+        Tasks.update(this._id, {
+            $set: {checked: !this.checked},
+        });
+    },
+    'click .delete'() {
+        Tasks.remove(this._id);
+    },
+    'click li'() {
+        var show = Template.instance().showDescription;
+        show.set(!show.get());
+    }
+});
+
+Template.task.helpers({
+    showMore: function(){
+        return Template.instance().showDescription.get();
+    }
+})
